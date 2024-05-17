8c01cf05f559a388392b92aca40d62ead740628b
Diana130613 <152212701+Diana130613@users.noreply.github.com>
2024-05-17 21:35:12 +0300

    Add files via upload

---
+++ task_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Time {
+private:
+    int hours;
+    int minutes;
+
+public:
+    Time(int h, int m) : hours(h), minutes(m) {}
+
+    string getInfo() {
+        stringstream ss;
+        ss << "Часы: " << hours << ", Минуты: " << minutes;
+        return ss.str();
+    }
+
+    void TotalMinutes() {
+        int totalMinutes = hours * 60 + minutes;
+        cout << "Общее количество минут: " << totalMinutes << endl;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int hours, minutes;
+
+    cout << "Введите количество часов: ";
+    cin >> hours;
+
+    cout << "Введите количество минут: ";
+    cin >> minutes;
+
+    Time timeObj(hours, minutes);
+
+    cout << timeObj.getInfo() << endl;
+    timeObj.TotalMinutes();
+
+    return 0;
+}
---
+++ task_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+class Rectangle 
+{
+private:
+    int x1, y1, x2, y2;
+public:
+    Rectangle() : x1(0), y1(0), x2(0), y2(0) {}
+    Rectangle(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2) {}
+    // деструктор для освобождения памяти
+    ~Rectangle() {
+        cout << "Объект класса Rectangle уничтожен" << endl;
+    }
+    int calculateSquare() {
+        int width = abs(x2 - x1);
+        int height = abs(y2 - y1);
+        return width * height;
+    }
+    string getInfo() {
+        ostringstream oss;
+        oss << "Прямоугольник с координатами (" << x1 << ", " << y1 << ") и (" << x2 << ", " << y2 << ")";
+        return oss.str();
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    Rectangle rectangle1(10, 10, 30, 30);
+    cout << "Площадь прямоугольника : " << rectangle1.calculateSquare() << endl;
+    cout << "Информация о прямоугольнике: " << rectangle1.getInfo() << endl;
+
+    return 0;
+}
